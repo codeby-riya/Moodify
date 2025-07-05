fb0c6076195e5dcc392320037ad4086601f34a49
codeby-riya <codebyriya@gmail.com>
2025-07-05 18:09:18 +0530

    update

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.combined_route import combined_bp
-from deepface import DeepFace
 import os
 
 app = Flask(__name__)
+
+# Allow requests from your React frontend (localhost during testing)
 CORS(app, origins=["https://moodify-jet.vercel.app"], supports_credentials=True)
+
+
+# Register combined route from your ML logic
 app.register_blueprint(combined_bp)
 
-DeepFace.build_model("Emotion")
-
+# Basic route to check server status
 @app.route('/')
 def index():
     return "Moodify backend is running"
