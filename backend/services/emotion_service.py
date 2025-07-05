52df9b4a4e15e1e7ca562000de569a8cb4c9af55
codeby-riya <codebyriya@gmail.com>
2025-07-06 01:53:50 +0530

    update

--- backend/services/emotion_service.py
+++ backend/services/emotion_service.py
@@ -1,9 +1,6 @@
 from deepface import DeepFace
 
 def detect_emotion(image_path):
-    try:
-        result = DeepFace.analyze(img_path=image_path, actions=["emotion"], enforce_detection=False)
-        mood = result[0]["dominant_emotion"].lower()
-        return mood
-    except Exception as e:
-        return f"Error: {str(e)}"
+    analysis = DeepFace.analyze(img_path=image_path, actions=['emotion'], enforce_detection=False)
+    mood = analysis[0]['dominant_emotion'].lower()
+    return mood
